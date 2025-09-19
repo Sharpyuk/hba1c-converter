f008ce60adb525effe87a43b4b24be289272470d
Craig Sharpe <Craig@Sharpestcomputers.co.uk>
2025-09-19 14:14:53 +0100

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dejunked.health',
   appName: 'Dejunked Health',
-  webDir: 'out',
-  bundledWebRuntime: false
+  webDir: 'out'
 };
 
 export default config;
